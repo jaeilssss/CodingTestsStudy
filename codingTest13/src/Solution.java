7c933e4696f9a9a69a070d058cfea873ceddc8fa
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-05-21 18:55:45 +0900

    5.21

--- codingTest13/src/Solution.java
+++ codingTest13/src/Solution.java
@@ -1,24 +1,32 @@
 
 public class Solution {
 
-        public int solution(int n, int[] lost, int[] reserve) {
-            int answer = n;
-            boolean result  = false;
-            for(int i = 0 ; i<lost.length ; i++){
-                for(int  j = 0 ; j<reserve.length ; j++){
-                    if(lost[i]-1 == reserve[j] || lost[i]+1==reserve[j]){
-                        result=true;
-                        reserve[j]=-1;
-                    }
-                }
-                if(!result){
-                    answer--;
-                }
-                result = false;
-            }
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
 
-                return answer;
+        int [] people = new int[n];
+
+        for(int l  : lost){
+            people[l-1]--;
         }
+        for(int r : reserve){
+            people[r-1]++;
+        }
+
+        for (int i = 0; i < people.length; i++) {
+            if(people[i] == -1) {
+                if(i-1>=0 && people[i-1] == 1) {
+                    people[i]++;
+                    people[i-1]--;
+                }else if(i+1< people.length && people[i+1] == 1) {
+                    people[i]++;
+                    people[i+1]--;
+                }else
+                    answer--;
+            }
+        }
+        return answer;
     }
+}
 
 
--- codingTest13/src/mainexe.java
+++ codingTest13/src/mainexe.java
@@ -2,7 +2,7 @@ public class mainexe {
 
     public static void main(String [] args){
         Solution solution = new Solution();
-        System.out.println(solution.solution(10, new int[]{2}, new int[]{1,3,5,7}));
+        System.out.println(solution.solution(10, new int[]{2,3}, new int[]{1,3,5,7}));
 
     }
 }
