2711d34ca9ee3bbf6d52bdda4ee2c2f2473ec42e
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-05-23 17:30:10 +0900

    kakaoBlindingCodingTest

--- codingTest14/src/Solution.java
+++ codingTest14/src/Solution.java
@@ -1,2 +1,43 @@
-public class Solution {
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer>[] stacks = new Stack<Integer>().toArray(new Stack[board[0].length]);
+        Stack<Integer> basket = new Stack<>();
+        for(int i = 0; i <board[0].length;i++){
+            stacks[i] = new Stack<>();
+            for(int j =board[0].length-1; j>=0; j--){
+                stacks[i].push(board[j][i]);
+            }
+        }
+        for(int  i = 0; i <moves.length ; i++){
+            if(!stacks[moves[i]-1].empty()){
+                while(true){
+                    int num = stacks[moves[i]-1].pop();
+                    if(num==0 && stacks[moves[i]-1].empty()){
+                        break;
+                    }
+                    if(num!=0){
+                        if(!basket.empty()){
+                            int temp = basket.pop();
+                            if(temp==num){
+                                answer = answer+2;
+                            }else{
+                                basket.push(temp);
+                                basket.push(num);
+                            }
+                            break;
+                        }else{
+                            basket.push(num);
+                            break;
+                        }
+                    }
+                }
+            }else{
+            }
+
+        }
+        return answer;
+    }
 }
--- codingTest14/src/mainexe.java
+++ codingTest14/src/mainexe.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class mainexe {
+    public static void main(String [] args){
+        Solution solution = new Solution();
+      System.out.println(solution.solution(new int[][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}},
+   new int[]{1,5,3,5,1,2,1,4}));
+    }
 }
