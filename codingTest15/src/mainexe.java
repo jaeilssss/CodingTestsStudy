238ae556631837c87d3a7eab3cfd688d8a78c0f4
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-05-25 22:47:39 +0900

    시저암호 문제풀기

---
+++ codingTest15/src/mainexe.java
@@ -0,0 +1,6 @@
+public class mainexe {
+    public static void main(String [] args){
+        Solution solution = new Solution();
+        System.out.println(solution.solution("Z a S",1));
+    }
+}
