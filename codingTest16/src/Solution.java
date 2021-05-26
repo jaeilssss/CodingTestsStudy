1fb70bb4b439abdb6ef50c1c1d40ed494c93ae10
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-05-26 18:59:01 +0900

    124나라 문제 푸는중

---
+++ codingTest16/src/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int n) {
+        return divide(n);
+    }
+
+    public String divide(int n){
+        String result ="";
+
+        if(n%3=){
+            return String.valueOf(4);
+        }
+       if(n%3==0 )
+        return result;
+    }
+}
---
+++ codingTest16/src/mainexe.java
@@ -0,0 +1,8 @@
+public class mainexe {
+
+    public static void main(String [] args){
+        Solution solution = new Solution();
+
+        System.out.println(solution.solution(9));
+    }
+}
