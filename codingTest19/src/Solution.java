b4a05dab00968625ab5858c216b0ed04788c22b4
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-06-05 17:00:35 +0900

    땅따먹기 문제

--- codingTest19/src/Solution.java
+++ codingTest19/src/Solution.java
@@ -1,82 +1,26 @@
 
-import sun.jvm.hotspot.memory.Universe;
-
-import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 class Solution {
-    int position ;
     public int solution(int[] priorities, int location) {
-        int answer= location+1;
-        position = location;
-
-       merge(0,priorities.length-1,priorities);
-        for(int i =0; i<priorities.length ; i++){
-            System.out.println(priorities[i]);
+        int answer= 1;
+        int  num = 0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int  i : priorities){
+            priorityQueue.offer(i);
         }
-        System.out.println(" ");
-        return position;
-    }
-    public int [] merge(int start, int end ,int [] priorities){
-        if(start<end){
-            int mid = (start+end)/2;
-            priorities = merge(start,mid,priorities);
-            System.out.println("first "+ position);
-            priorities = merge(mid+1,end,priorities);
-            System.out.println("second " + position);
-            priorities = sort(start,mid,end,priorities);
-            System.out.println("third "+ position);
-        }
-
-        return priorities;
-    }
-    public int [] sort(int start , int mid , int end , int [] priorities){
-        int leftStart = start;
-        int rightStart = mid+1;
-        int i = start;
-        int [] temp = new int[priorities.length];
-        while (true){
-            if(priorities[leftStart]<priorities[rightStart]){
-                temp[i++] = priorities[rightStart];
-                if(rightStart==position){
-                    position = i-1;
-                    rightStart++;
-                }else{
-                    rightStart++;
-                }
-            }else if(priorities[leftStart]>=priorities[rightStart]){
-                temp[i++] = priorities[leftStart];
-                if(leftStart==position){
-                    position=i-1;
-                    leftStart++;
-                }else{
-                    leftStart++;
-                }
-            }if(leftStart>mid){
-                for(int num = rightStart ;num<=end;num++){
-                    if(num==position){
-                        position=i;
+        while(!priorityQueue.isEmpty()){
+            for(int  i = 0; i<priorities.length;i++){
+                if(priorities[i]==priorityQueue.peek()) {
+                    if (i == location) {
+                        return answer;
                     }
-                    temp[i++] = priorities[num];
+                    priorityQueue.poll();
+                    answer++;
                 }
-                for(int num = start ; num<=end ; num++){
-                    priorities[num] = temp[num];
-                }
-                return priorities;
-            }else if(rightStart>end){
-                for(int  num = leftStart ; num <=mid;num++){
-                    if(num==position){
-                        position=i;
-                    }
-
-                    temp[i++] = priorities[num];
-                }
-                for(int num = start; num<=end; num++){
-                    priorities[num] = temp[num];
-                }
-                return priorities;
             }
         }
-
+        return answer;
     }
-
 }
--- codingTest19/src/mainexe.java
+++ codingTest19/src/mainexe.java
@@ -1,6 +1,6 @@
 public class mainexe {
     public static void main(String [] args){
         Solution solution = new Solution();
-        System.out.println(solution.solution(new int[]{1,2,1,4,1},0));
+        System.out.println(solution.solution(new int[]{1,2,3,7,5,6},4));
     }
 }
